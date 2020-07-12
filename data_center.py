ea43005209e30c1b4e8721bb922051b939b11e28
Aman Yadav <64650224+Aman-Yadav-PY@users.noreply.github.com>
2020-07-12 20:33:21 +0530

    Update data_center.py

--- data_center.py
+++ data_center.py
@@ -1,46 +1,59 @@
 import sqlite3
 import os
-print(os.getcwd())
 
-connection = sqlite3.connect('user_data.db')
+class database:
+	def __init__(self, filename):
+		self.filename = filename
+		
+	def initialize(self):
+		self.connection = sqlite3.connect(self.filename)
+		self.navigate = self.connection.cursor()
 
-navigation = connection.cursor()
-navigation.execute("""CREATE TABLE userdata(
-					Name text, 
-					Email text, 
-					Gender text, 
-					age integer, 
-					birthday text,
-					password text
-	                              )""")
-def insert_data(data):
-	with connection:
-		navigation.execute("""INSERT INTO userdata VALUES(:Name, :Email, :Gender, :age, :birthday, :password)""",
-			{'Name':data.name, 'Email':data.email, 'Gender':data.gender, 'age':data.age, 'birthday':data.birthday,'password':data.password})
+		database_name = 'user data.db'
+		# if database_name in [x for x in os.listdir()]: 
+		self.navigate.execute("""CREATE TABLE userdata(
+			Name text, 
+			Email text, 
+			Gender text, 
+			age integer, 
+			birthday text,
+			password text
 
-def fetcher(data):
-	navigation.execute("SELECT * FROM userdata WHERE Name= :Name", {'Name':data})
-	x = navigation.fetchone()
-	return x
+			)""")
+		# else:
+		# 	pass
 
-class data:
-	def __init__(self, name, email, gender, age, birthday, password):
-		self.name = name
-		self.email = email
-		self.gender = gender
-		self.age = age
-		self.birthday = birthday
-		self.password = password
+	def insert_data(self, data):
+		with self.connection:
+			self.navigate.execute("""INSERT INTO userdata VALUES(
+				:Name, :Email, :Gender, :age, :birthday, :password)""",
+				{'Name':data.name, 'Email':data.email, 'Gender':data.gender, 
+				'age':data.age, 'birthday':data.birthday, 'password':data.password})
 
-user1 = data('Aman Yadav', 'assassins2032004@gmail.com', 'male', 17, '20 April 2003', '1234')
-user2 = data('Kuldeep Yadav', 'kuldeep2032004@gmail.com', 'male', 17, '20 March 2003', '12345')
+	def fetcher(self, name):
+		self.navigate.execute("SELECT * FROM userdata WHERE Name=:Name", {'Name':name})
+		self.retrived_data = self.navigate.fetchone()
+		return self.retrived_data
 
-insert_data(user1)
-insert_data(user2)
+if __name__ == '__main__':
+	db = database(":memory:")
+	db.initialize()
+	class data:
+		def __init__(self, name, email, gender, age, birthday, password):
+			self.name = name
+			self.email = email
+			self.gender = gender
+			self.age = age
+			self.birthday = birthday
+			self.password = password
+	user1 = data('Aman', 'aman1234@gmail.com', 'male', 17, '20 April 2003', '1234')
+	user2 = data('Kuldeep', 'kuldeep2032004@gmail.com', 'male', 17, '20 March 2003', '12345')
 
-print(fetcher('Kuldeep Yadav')[1])
+	db.insert_data(user1)
+	db.insert_data(user2)
 
-connection.close()
+	print(db.fetcher('Kuldeep Yadav'))
 
 
 
+		
